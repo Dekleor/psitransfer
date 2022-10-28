6e701c150bed6c0fb719b9437589b3377c6d7787
Dekleor <donier.matteo@gmail.com>
2022-10-28 13:44:41 +0200

    tatonner

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -31,10 +31,10 @@
           table.table.table-hover.table-striped.files
             tbody
               tr(v-for='file in files' , style='cursor: pointer' , @click='download(file)' )
-                template(v-if="humanFileSize(file.size) != file.metadata.size")
+                template(v-if="humanFileSize(file.size) != file.size")
                   td
                     strong {{ $root.lang.ongoingDownload }}
-                template(v-else="humanFileSize(file.size) == file.metadata.size")
+                template(v-else="humanFileSize(file.size) == file.size")
                 td.file-icon
                   file-icon(:file='file' )
                 td
